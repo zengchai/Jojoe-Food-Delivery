ba8f54c20a3723d30372ac27977aa8b0818a56e2
“Loy” <tanzenchai0923@gmail.com>
2023-06-12 22:11:50 +0800

    Adding php for authentication

---
+++ php/guest_form.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<?php
+session_start();
+
+// If the user is not logged in send him/her to the login form
+if(isset($_SESSION["Login"])){
+if ($_SESSION["Login"] == "NO") {
+    echo "hi";
+}
+
+unset($_SESSION["Login"]);}
+?>
+<title>Login</title>
+</head>
+<body>
+<form method="post" action="check_login.php">
+<p>Username: <input type="text" name="username" /></p>
+<p>Password: <input type="password" name="password" /></p>
+<p><input type="submit" value="Let me in" /></p>
+
+</form>
+</body>
+</html>
